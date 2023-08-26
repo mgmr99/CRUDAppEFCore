984dcd01a6c1b4b4c4a02ed7e4e3e0157848f4eb
Madhu Ghimire <ghimiremadhu2058@gmail.com>
2023-08-26 17:47:53 +0545

    first commit

---
+++ RazorCRUD/Model/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace RazorCRUD.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public DateTime DOB { get; set; }
+    }
+}
